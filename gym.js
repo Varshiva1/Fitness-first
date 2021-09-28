a73d45d836656c6301e35817f4e888ff3fb69d98
Varshiva1 <shivamvarun75@gmail.com>
2021-09-28 13:30:48 +0530

    code updated

---
+++ gym.js
@@ -0,0 +1,12 @@
+var name1= document.querySelector("#name");
+var age= document.querySelector("#age");
+var profession= document.querySelector("#profession");
+var button= document.querySelector("#submitbtn");
+
+
+//console.log(document.querySelector("#name").value);
+button.addEventListener("click",function check(){
+console.log(name1.value);
+console.log(age.value);
+console.log(profession.value)
+});
